8bdc8ed0040d67be2205df8733c23fb760271f37
Muhammad Sulthan Mazaya <73002733+MSMazaya@users.noreply.github.com>
2022-03-23 05:12:50 +0700

    feature: add working app router and initial baseline of homeview, also added ws package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile_app/app/app.router.dart';
 import 'package:stacked_services/stacked_services.dart';
-// import 'package:transitspot/app/app.locator.dart';
 import 'app/app.dart';
 
 void main() async {
--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -1,14 +1,21 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile_app/ui/home/home_viewmodel.dart';
+import 'package:stacked/stacked.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(),
+    return ViewModelBuilder<HomeViewModel>.reactive(
+      builder: (context, model, child) => Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          alignment: Alignment.center,
+          child: Text(model.text),
+        ),
+      ),
+      viewModelBuilder: () => HomeViewModel(),
     );
   }
 }
